b938f51aa86ab7fa6f4094d17315c8001d38f7ed
Hernan M. Settino <settinoh@Hernans-MBP.fibertel.com.ar>
2023-11-30 09:41:51 -0300

    new ejer

---
+++ ejercicios/ejercicio2.go
@@ -0,0 +1,33 @@
+package ejercicios
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func ListadeNumeros() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var numero int
+	var err error
+
+	for {
+		fmt.Println("Ingrese Numero: ")
+		if scanner.Scan() {
+			numero, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				continue
+			} else {
+				break
+			}
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("%d x %d = %d", numero, i, i*numero)
+
+	}
+}
